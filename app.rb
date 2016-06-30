ab25d922d45cd1cb6fd3badadbd5f1cc48502537
supergivi <chazoff@gmail.com>
2016-06-30 23:45:18 +0300

    s

--- app.rb
+++ app.rb
@@ -1,42 +1,29 @@
 require 'sinatra'
 require 'json'
-require 'ohm'
 set :bind, '0.0.0.0'
 
 
 get '/room.json' do
-  room = Room.find(name: 'room').first
-  if room.nil?
-    room = Room.create(name: 'room', current_temperature: 22, set_temperature: 23)
+  if !File.exist?('data.json')
+    File.open("data.json", 'w') {|f| f.write({current_temperature: 22, set_temperature: 23}.to_json) }
   end
+  file = File.open("data.json", 'r')
   content_type :json
-  room.to_hash.to_json
+  data = file.read
+  file.close
+  data
 end
 
 post '/room.json' do
-  room = Room.find(name: 'room').first
-  room.update(set_temperature: JSON.parse(request.env["rack.input"].read)['set_temperature'])
+  file = File.open("data.json", 'r')
+  data = JSON.parse(file.read)
+  file.close
+  data['set_temperature'] = JSON.parse(request.env["rack.input"].read)['set_temperature']
+  File.open("data.json", 'w') {|f| f.write(data.to_json) }
   content_type :json
   {result: :ok}.to_json
 end
 
-post 'rooms' do
-  room = Room.find(name: params[:room]).first
-  if room.nil?
-    room = Room.create(name: params[:room])
-  end
-  room.update
-end
 
-class Room < Ohm::Model
-  attribute :name
-  attribute :current_temperature
-  attribute :set_temperature
-  unique :name
 
-  def to_hash
-    { name: name, current_temperature: current_temperature, set_temperature: set_temperature }
-  end
 
-  index :name
-end
